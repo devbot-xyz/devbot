c73865fdc8be41545735ecfa2ce0d0b283f5cbdf
Venkata krishna Sundararajan <vkvenkat94@gmail.com>
2016-12-11 02:15:41 +0530

    add listener and send reply

--- dbconfig/db.js
+++ dbconfig/db.js
@@ -1,8 +1,9 @@
 var Sequelize = require('sequelize')
 
 module.exports = function (cb) {
-  global.sequelize = new Sequelize('devdb', 'XX', 'XXX', {
-      host: '172.17.0.29',
+  //   global.sequelize = new Sequelize('devdb', 'postgres', 'mysecretpassword', {
+  global.sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER , process.env.DB_PASS, {
+      host: process.env.DB_HOST,
       dialect: 'postgres',
       pool: {
         max: 5,
