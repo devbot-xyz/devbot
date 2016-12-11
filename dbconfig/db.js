0fd836b8b833090a5f313ad3fd5fd0c2cf856f69
Venkata krishna Sundararajan <vkvenkat94@gmail.com>
2016-12-11 09:56:48 +0530

    add process env
    Signed-off-by: Venkata krishna Sundararajan <vkvenkat94@gmail.com>

--- dbconfig/db.js
+++ dbconfig/db.js
@@ -3,7 +3,8 @@ var Sequelize = require('sequelize')
 module.exports = function (cb) {
   //  global.sequelize = new Sequelize('devdb', 'postgres', 'mysecretpassword', {
     global.sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER , process.env.DB_PASS, {
-      host: '172.17.0.2',
+  //    host: '172.17.0.2',
+    host: process.env.DB_HOST,
       dialect: 'postgres',
       pool: {
         max: 5,
